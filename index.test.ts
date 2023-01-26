1a3f4bd808270ff268caf770d26223869f7e9d7d
石渡拓海 <takumi-ishiwata@c-fo.com>
2023-01-27 00:30:02 +0900

    first commit

---
+++ index.test.ts
@@ -0,0 +1,11 @@
+import { Hono } from "https://deno.land/x/hono/mod.ts";
+import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+
+Deno.test("Hello, World", async () => {
+  const app = new Hono();
+  app.get("/", (c) => c.text("Hello! Hono!"));
+
+  const res = await app.request("http://localhost/");
+  assertEquals(res.status, 200);
+  assertEquals(await res.text(), "Hello! Hono!");
+});
