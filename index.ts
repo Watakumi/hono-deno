650c658bc552d47cc119541b52694f32fd26bb04
石渡拓海 <takumi-ishiwata@c-fo.com>
2023-01-27 00:54:40 +0900

    Add My Profile

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
 import { serve } from "https://deno.land/std/http/server.ts";
 import { Hono } from "https://deno.land/x/hono/mod.ts";
+import { prettyJSON } from "https://deno.land/x/hono/middleware.ts";
+import { myProfile } from "./myprofile.ts";
 const app = new Hono();
 
+app.use("*", prettyJSON());
 app.get("/", (c) => c.text("Hello! Hono!"));
+app.get("/myprofile", (c) => c.json(myProfile));
 
 serve(app.fetch);
---
+++ myprofile.ts
@@ -0,0 +1,9 @@
+export const myProfile = {
+  name: "Watakumi",
+  description: ["Ruby Developer"],
+  languages: {
+    skills: ["ruby", "javascript"],
+    uses: ["Ruby on Rails", "reactjs", "nextjs", "deno"],
+  },
+  challenges: ["シンプルで楽しいアプリを開発したい！"],
+};
